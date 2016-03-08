3c28c6a283c63ad18e1e0a07e42be3e7d3b40285
Jordan Bourque <Jordan@Jordans-MBP.v35.lan>
2016-03-07 19:31:39 -0500

    Made major changes and it is now slightly usable

--- msp430.cpp
+++ msp430.cpp
@@ -1,58 +1,94 @@
 #include <iostream>
-#include <string>
 #include <fstream>
+#include <string>
 using namespace std;
 
 enum mnem {
 	UNDEFINED = 0, RRC, SWRB, RRA,
 	SXT, PUSH, CALL, RETI,
-	JNE, JEQ, JNC, JC,
-	JN, JGE, JL, JMP,
-	MOV, ADD, ADDC, SUBC,
+	NOTUSED,
+
+	JNE = 10, JEQ, JNC, JC,
+	JN, JGE, JL, JMP, 	
+
+	MOV = 20, ADD, ADDC, SUBC,
 	SUB, CMP, DADD, BIT,
 	BIC, BIS, XOR, AND,
-	NOP
+
+	NOP = 35, POP, BR, RET,
+	CLRC, SETC, CLRZ, SETZ,
+	CLRN, SETN, DINT, EINT,
+	RLA, RLC, INV, CLR,
+	TST, DEC, DECD, INC,
+	INCD, ADC, DADC, SBC,
+	ERROR
 };
 
+string map[] = {
+	"undefined", "rrc", "swrb", "rra",
+	"sxt", "push", "call", "reti",
+	"notused", "9"
+
+	"jne", "jeq", "jnc", "jc",
+	"jn", "jge", "jl", "jmp",
+	"18", "19",
+
+	"mov", "add", "addc", "subc",
+	"sub", "cmp", "dadd", "bit",
+	"bic", "bis", "xor", "and",
+	"32","33","34"
+
+	"nop", "pop", "br", "ret",
+	"clrc", "setc", "clrz", "setz",
+	"clrn", "setn", "dint", "eint",
+	"rla", "rlc", "inv", "clr",
+	"tst", "dec", "decd", "inc",
+	"incd", "adc", "dadc", "sbc",
+	"error"
+};
+
+
+
 enum registers {
-	PC, SP, CG1, CG2,
+	PC, SP, SR, CG,
 	R4, R5, R6, R7,
 	R8, R9, R10, R11,
 	R12, R13, R14, R15
 };
 
-enum types {
-	dest, src, dest_reg,
-	src_reg, label
-};
-
 enum addressing_mode
 {
-	register_mode, indexed_mode, symbolic_mode, absolute_mode,
+	register_mode = 0, indexed_mode, symbolic_mode, absolute_mode,
 	indirect_register_mode, indirect_autoincrement, immediate_mode,
-	constant[6] = {-1, 0, 1, 2, 4, 8}, error
+	constantneg1, constant0, constant1, constant2, constant4,
+	constant8, error
 };
 
 struct instruction
 {
 	struct operand
 	{
-		types type;
-		registers reg;
+		registers source_reg;
+		registers dest_reg;
 		uint16_t imm;
+		mnem instruction;
 	}op[3];
-	mnem instruction;
-	addressing_mode mode;
-}set;
+	bool bw;
+	addressing_mode destination;
+	addressing_mode source;
+	bool error;
+	int optype;
+};
 
-int msp430Decompose(int16_t value)
+int msp430Decompose(uint16_t value[3], instruction &set)
 {
+	int amount_used = 0;
 	union {
 		uint16_t full;
 		struct 
 		{
-			registers dest_reg:4;
-			uint16_t ad:2;
+			registers source_reg:4;
+			uint16_t as:2;
 			uint16_t bw:1;
 			mnem opcode:3;
 			uint16_t bitset:6;
@@ -73,265 +109,409 @@ int msp430Decompose(int16_t value)
 			mnem opcode:4;
 		}op3;
 	}decode;
-	decode.full = value;
-	enum mnem2{
-		MOV = 4, ADD, ADDC, SUBC,
-		SUB, CMP, DADD, BIT,
-		BIC, BIS, XOR, AND
-	};
-	switch ((decode.op3.opcode))
+	decode.full = value[0];
+
+	set.error = false;
+	if (decode.op1.bw == 0)
 	{
-		case 0: cout << "error\n";
+		set.bw = false;
+	}
+	else
+	{
+		set.bw = true;
+	}
+
+	if (decode.op3.opcode == 1)
+	{
+		set.optype = 1;
+	}
+	else if (decode.op3.opcode == 2 || decode.op3.opcode == 3)
+	{
+		set.optype = 2;
+	}
+	else if (decode.op3.opcode >=4)
+	{
+		set.optype = 3;
+	}
+	switch (set.optype)
+	{
+		case 0:
+			set.error = true;
 			break;
 		case 1:
-			switch(decode.op3.as)
+			set.op[0].instruction = decode.op1.opcode;
+			set.op[0].source_reg = decode.op1.source_reg;
+			switch(decode.op1.as)
 			{
 				case 0:
-					if (decode.op3.ad == 0)
-					{
-						set.addressing_mode = register_mode;
-					}
-					else
-					{
-						set.addressing_mode = error;
-					}
+					if (decode.op1.source_reg != 3)
+						set.source = register_mode;
+					else if (decode.op1.source_reg == 3)
+						set.source = constant0;
 					break;
 				case 1:
-					if (decode.op3.ad == 1)
-					{
-						if (decode.op3.dest_reg != PC && decode.op3.dest_reg != SR &&
-							decode.op3.dest_reg != CG1)
-						{
-							set.addressing_mode = indexed_mode;
-						}
-						if (decode.op3.dest_reg == PC)
-						{
-							set.addressing_mode = symbolic_mode;
-						}
-						if (decode.op3.dest_reg == SR)
-						{
-							set.addressing_mode = absolute_mode;
-						}
-					}
+					if (decode.op1.source_reg == PC)
+						set.source = symbolic_mode;
+					else if (decode.op1.source_reg == SR)
+						set.source = absolute_mode;
+					else if (decode.op1.source_reg == CG)
+						set.source = constant1;
+					else
+						set.source = indexed_mode;
+					break;
+				case 2:
+					if (decode.op1.source_reg == SR)
+						set.source = constant4;
+					else if (decode.op1.source_reg == CG)
+						set.source = constant2;
+					else
+						set.source = indirect_register_mode;
+					break;
+				case 3:
+					if (decode.op1.source_reg == PC)
+						set.source = immediate_mode;
+					else if (decode.op1.source_reg == SR)
+						set.source = constant8;
+					else if (decode.op1.source_reg == CG)
+						set.source = constantneg1;
+					else
+						set.source = indirect_autoincrement;
+					break;
+				default: break;
 			}
-			set.instruction = decode.op1.opcode;
-			set.op[0].type = dest_reg;
-			set.op[0].reg = decode.op1.dest_reg;
 			break;
 		case 2:
+			set.op[1].instruction = (mnem)(decode.op2.condition + JNE);
+			set.op[1].imm = decode.full;
+			break;
 		case 3:
-			set.instruction = (mnem)(decode.op2.condition + RETI);
-			set.op[0].type = label;
-			set.op[0].imm = decode.full;
+			switch(decode.op1.as)
+			{
+				case 0:
+					if (decode.op3.source_reg != 3)
+						set.source = register_mode;
+					else if (decode.op3.source_reg == 3)
+						set.source = constant0;
+					break;
+				case 1:
+					if (decode.op3.source_reg == PC)
+						set.source = symbolic_mode;
+					else if (decode.op3.source_reg == SR)
+						set.source = absolute_mode;
+					else if (decode.op3.source_reg == CG)
+						set.source = constant1;
+					else
+						set.source = indexed_mode;
+					break;
+				case 2:
+					if (decode.op3.source_reg == SR)
+						set.source = constant4;
+					else if (decode.op3.source_reg == CG)
+						set.source = constant2;
+					else
+						set.source = indirect_register_mode;
+					break;
+				case 3:
+					if (decode.op3.source_reg == PC)
+						set.source = immediate_mode;
+					else if (decode.op3.source_reg == SR)
+						set.source = constant8;
+					else if (decode.op3.source_reg == CG)
+						set.source = constantneg1;
+					else
+						set.source = indirect_autoincrement;
+					break;
+				default: break;
+			}
+			if (decode.op3.ad == 0)
+			{
+				set.destination = register_mode;
+			}
+			else
+			{
+				if (decode.op3.dest_reg == 0)
+					set.destination = symbolic_mode;
+				else if (decode.op3.dest_reg == 2)
+					set.destination = absolute_mode;
+				else if (decode.op3.dest_reg != 3)
+					set.destination = indexed_mode;
+				else
+					set.destination = error;
+			}
+			set.op[2].instruction = (mnem)(decode.op3.opcode + (MOV-4));
+			set.op[2].source_reg = decode.op3.source_reg;
+			set.op[2].dest_reg = decode.op3.dest_reg;
+			if (set.op[2].instruction == MOV)
+			{
+				if (set.op[2].source_reg == set.op[2].dest_reg && set.source == register_mode
+					&& set.destination == register_mode)
+				{
+					set.op[2].instruction = NOP;
+				}
+				else if (set.source == constant0 && 
+						 set.destination == register_mode && set.op[2].dest_reg == SR)
+				{
+					set.op[2].instruction = NOP;
+				}
+				if (set.source == indirect_autoincrement && set.op[2].source_reg == SP &&
+					set.destination == register_mode && set.op[2].dest_reg == PC)
+				{
+					set.op[2].instruction = RET;
+				}
+				else if (set.source == indirect_autoincrement && set.op[2].source_reg == SP)
+				{
+					set.op[2].instruction = POP;
+				}
+				if (set.destination == register_mode && set.op[2].dest_reg == PC)
+				{
+					set.op[2].instruction = BR;
+				}
+			}
+			else if (set.op[2].instruction == BIC || set.op[2].instruction == BIS)
+			{
+				if (set.destination == register_mode && set.op[2].dest_reg == SR
+					&& set.op[2].instruction == BIC)
+				{
+					if (set.source == constant1)
+						set.op[2].instruction = CLRC;
+					else if (set.source == constant2)
+						set.op[2].instruction = CLRZ;
+					else if (set.source == constant4)
+						set.op[2].instruction = CLRN;
+					else if (set.source == constant8)
+						set.op[2].instruction = DINT;
+				}
+				else if (set.destination == register_mode && set.op[2].dest_reg == SR
+					&& set.op[2].instruction == BIS)
+				{
+					if (set.source == constant1)
+						set.op[2].instruction = SETC;
+					else if (set.source == constant2)
+						set.op[2].instruction = SETZ;
+					else if (set.source == constant4)
+						set.op[2].instruction = SETN;
+					else if (set.source == constant8)
+						set.op[2].instruction = EINT;
+				}
+			}
+			else  if (set.op[2].instruction == ADD)
+			{
+				if (set.source == register_mode && set.destination == register_mode &&
+					set.op[2].source_reg == set.op[2].dest_reg)
+				{
+					set.op[2].instruction = RLA;
+				}
+				else if (set.source == constant1)
+				{
+					set.op[2].instruction = INC;
+				}
+				else if (set.source == constant2)
+				{
+					set.op[2].instruction = INCD;
+				}
+			}
+			else if (set.op[2].instruction == ADDC)
+			{
+				if (set.source == register_mode && set.destination == register_mode &&
+					set.op[2].source_reg == set.op[2].dest_reg)
+				{
+					set.op[2].instruction = RLC;
+				}
+				else if (set.source == constant0)
+				{
+					set.op[2].instruction = ADC;
+				}
+			}
+			else if (set.op[2].instruction == SUBC || set.op[2].instruction == XOR ||
+					 set.op[2].instruction == MOV || set.op[2].instruction == CMP ||
+					 set.op[2].instruction == SUB || set.op[2].instruction == DADD)
+			{
+				if (set.source == constantneg1 && set.op[2].instruction == XOR)
+				{
+					set.op[2].instruction = INV;
+				}
+				else if (set.source == constant0 && set.op[2].instruction == MOV)
+				{
+					set.op[2].instruction = CLR;
+				}
+				else if (set.source == constant0 && set.op[2].instruction == CMP)
+				{
+					set.op[2].instruction = TST;
+				}
+				else if (set.source == constant1 && set.op[2].instruction == SUB)
+				{
+					set.op[2].instruction = DEC;
+				}
+				else if (set.source == constant2 && set.op[2].instruction == SUB)
+				{
+					set.op[2].instruction = DECD;
+				}
+				else if (set.source == constant0 && set.op[2].instruction == DADD)
+				{
+					set.op[2].instruction = DADC;
+				}
+				else if (set.source == constant0 && set.op[2].instruction == SUBC)
+				{
+					set.op[2].instruction = SBC;
+				}
+			}
 			break;
-		case MOV:
-			if (decode.op3.source_reg == decode.op3.dest_reg && decode.op3.source_reg >= 3 &&
-				decode.op3.opcode == MOV)
-			{
-				set.instruction = NOP;
-				break;
-			}
-			else if (decode.op3.opcode == MOV && decode.op3.as == 0 && decode.op3.source_reg == 3)
-			{
-				set.instruction = CLR;
-				set.op[0].type = dest_reg;
-				set.op[0].reg = decode.op3.dest_reg;
-				break;
-			}
-			else if (decode.op3.as == 3 && decode.op3.source_reg != 0 &&
-					 decode.op3.source_reg != 2 && decode.op3.source_reg != 3)
-			{
-				if (decode.op3.dest_reg == PC)
-				{
-					set.instruction = RET;
-					break;
-				}
-				else
-				{
-					set.instruction = POP;
-					set.op[0].type = dest_reg;
-					set.ep[0].reg = decode.op3.dest_reg;
-					break;
-				}
-			}
-			else if (decode.op3.opcode == MOV && decode.op3.source_reg == PC &&
-					 decode.op3.source_reg == PC)
-			{
-				set.instruction = BR;
-				set.op[0].type = dest_reg;
-				set.op[0].reg = decode.op3.dest_reg;
-				break;
-			}
-
-		case ADD:
-			if (decode.op3.opcode == ADD && (decode.op3.as == 1 || decode.op3.as == 2)
-				&& decode.op3.source_reg == 3)
-			{
-				if (decode.op3.as == 1)
-				{
-					set.instruction = INC;
-					set.op[0].type = dest_reg;
-					set.op[0].reg = decode.op3.dest_reg;
-					break;
-				}
-				else
-				{
-					set.instruction = INCD;
-					set.op[0].type = dest_reg;
-					set.op[0].reg = decode.op3.dest_reg;
-					break;
-				}
-			}
-		case ADDC:
-			if (decode.op3.source_reg == decode.op3.dest_reg && (decode.op3.opcode == ADD ||
-				decode.op3.opcode == ADDC))
-			{
-				if (decode.op3.opcode == ADD)
-					set.instruction = RLA;
-				else
-					set.instruction = RLC;
-
-				set.op[0].type = dest_reg;
-				set.op[0].reg = decode.op3.dest_reg;
-				break;
-			}
-			else if (decode.op3.opcode == ADDC && decode.op3.as == 0 && decode.op3.source_reg == 3)
-			{
-				set.instruction = ADC;
-				set.op[0].type = dest_reg;
-				set.op[0].reg = decode.op3.dest_reg;
-			}
-
-		case SUBC:
-			if (decode.op3.opcode == SUBC && decode.op3.as == 0 && decode.op3.source_reg == 3)
-			{
-				set.instruction = SBC;
-				set.op[0].type = dest_reg;
-				set.op[0].reg = decode.op3.dest_reg;
-			}
-		case SUB:
-			if (decode.op3.opcode == SUB && (decode.op3.as == 1 || decode.op3.as == 2)
-				&& decode.op3.source_reg == 3)
-			{
-				if (decode.op3.as == 1)
-				{
-					set.instruction = DEC;
-					set.op[0].type = dest_reg;
-					set.op[0].reg = decode.op3.dest_reg;
-					break;
-				}
-				else
-				{
-					set.instruction = DECD;
-					set.op[0].type = dest_reg;
-					set.op[0].reg = decode.op3.dest_reg;
-					break;
-				}
-			}
-		case CMP:
-			if (decode.op3.opcode == CMP && decode.op3.as == 0 && decode.op3.source_reg == 3)
-			{
-				set.instruction = TST;
-				set.op[0].type = dest_reg;
-				set.op[0].reg = decode.op3.dest_reg;
-				break;
-			}
-		case DADD:
-			if (decode.op3.opcode == DADD && decode.op3.as == 0 && decode.op3.source_reg == 3)
-			{
-				set.instruction = DADC;
-				set.op[0].type = dest_reg;
-				set.op[0].reg = decode.op3.dest_reg;
-			}
-		case BIT:
-		case BIC:
-		case BIS:
-			if (decode.op3.dest_reg == SP && (decode.op3.opcode == BIC || decode.op3.opcode == BIS))
-			{
-				switch(decode.op3.as)
-				{
-					case 1:
-						if (decode.op3.source_reg == 3)
-						{
-							if (decode.op3.opcode == BIC)
-								set.instruction = CLRC;
-							else
-								set.instruction = SETC;
-						}
-						break;
-					case 2:
-						if (decode.op3.source_reg == 2)
-						{
-							if (decode.op3.opcode == BIC)
-								set.instruction = CLRN;
-							else
-								set.instruction = SETN;
-						}
-						else if (decode.op3.source_reg == 3)
-						{
-							if (decode.op3.opcode == BIC)
-								set.instruction = CLRZ;
-							else
-								set.instruction = SETZ;
-						}
-						break;
-					case 3:
-						if (decode.op3.source_reg == 2)
-						{
-							if (decode.op3.opcode == BIC)
-								set.instruction = DINT;
-							else
-								set.instruction = EINT;
-						}
-						break;
-					default: break;
-
-				}
-				break;
-			} 
-		case XOR:
-			if (decode.op3.opcode == XOR && decode.op3.as == 3 && decode.op3.source_reg == 3)
-			{
-				set.instruction = INV;
-				set.op[0].type = dest_reg;
-				set.op[0].reg = decode.op3.dest_reg;
-				break;
-			}
-		case AND:
-			set.instruction = (mnem)(decode.op3.opcode + JMP);
-			set.op[0].type = src_reg;
-			set.op[0].reg = decode.op3.source_reg;
-			set.op[1].type = dest_reg;
-			set.op[1].reg = decode.op3.dest_reg;
-			
-			break;
-		default: cerr << "ERROR 1\n"; return -1;
+		default: set.op[set.optype-1].instruction = ERROR;
 			break;
 	}
-		return 0;
+	switch (set.source)
+	{
+		case register_mode:
+		case indirect_register_mode:
+		case indirect_autoincrement:
+			if (set.destination == register_mode || set.optype == 1)
+				amount_used = 2;
+			else
+				amount_used = 4;
+			break;
+		case immediate_mode:
+		case indexed_mode:
+		case symbolic_mode:
+		case absolute_mode:
+			if (set.destination == register_mode || set.optype == 1)
+				amount_used = 4;
+			else
+				amount_used = 6;
+			break;
+		default: 
+			amount_used = 2;
+			break;
+	}
+	return amount_used;
+}
+
+int output(uint16_t num[3], uint used, instruction& set)
+{
+//	swab(&code[(6-bytes_used)/2],&code[(6-bytes_used)/2], bytes_used);
+	for (int x = 0; x < used/2; x++)
+	{
+		cout << hex << num[x] << " ";
+	}
+	cout << "\t" << dec << map[set.op[set.optype-1].instruction-1];
+	switch(set.optype)
+	{
+		case 1:
+		case 3:
+			switch(set.source)
+			{
+				case register_mode:
+					if (set.optype == 3)
+						cout << "\tr" << set.op[2].source_reg;	
+					else
+						cout << "\tr" << set.op[0].source_reg;
+					break;
+				case indexed_mode:
+					if (set.optype == 3)
+						cout << "\tx(r" << set.op[2].source_reg << ")";	
+					else
+						cout << "\tx(r" << set.op[0].source_reg << ")";
+					break;
+				case symbolic_mode:
+					cout << "\t0x" << num[1];
+					break;
+				case absolute_mode:
+					cout << "\t&0x" << num[1];
+					break;
+				case indirect_register_mode:
+					if (set.optype == 3)
+						cout << "\t@r" << set.op[2].source_reg;
+					else
+						cout << "\t@r" << set.op[0].source_reg;
+					break;
+				case indirect_autoincrement:
+					if (set.optype == 3)
+						cout << "\t@r" << set.op[2].source_reg << "+";
+					else
+						cout << "\t@r" << set.op[0].source_reg << "+";
+					break;
+				case immediate_mode:
+					cout << "\t#0x" << num[1];
+					break;
+				case constantneg1:
+					cout << "\t#-1";
+					break;
+				case constant0:
+					cout << "\t#0";
+					break;
+				case constant1:
+					cout << "\t#1";
+					break;
+				case constant2:
+					cout << "\t#2";
+					break;
+				case constant4:
+					cout << "\t#4";
+					break;
+				case constant8:
+					cout << "\t#8";
+					break;
+				case error:
+					cout << "error, op not valid.";
+					break;
+				default: cout << "error, op not valid.";
+					break;
+			}
+			if (set.optype == 3)
+			{
+				switch(set.destination)
+				{
+					case register_mode:
+						cout << ",\tr" << set.op[2].dest_reg;
+						break;
+					case indexed_mode:
+						cout << ",\tx(r" << set.op[2].dest_reg << ")";
+						break;
+					case symbolic_mode:
+						cout << ",\t0x" << num[1];
+						break;
+					case absolute_mode:
+						cout << ",\t&0x" << num[1];
+						break;
+					default: cout << ", \terror, op not valid.";
+						break;
+				}
+			}
+			cout << endl;
+			break;
+		case 2:
+			cout << &num[0] << endl;
+		default: break;
+	}
+	return 0;
 }
 
 int main()
 {
-	ifstream textfile ("helloworld.txt");
+	ifstream textfile ("hi.bin", ios::binary);
 	textfile.seekg(0,textfile.end);
 	size_t size = textfile.tellg();
 	textfile.seekg(0, textfile.beg);
-	if (size == 0)
+	if (size <= 1)
 	{
 		cerr << "Invalid\n";
 		return -1;
 	}
-	int16_t code = 0;
-	for (int x = 0; x < size/2; x += 2)
+	uint16_t code[3] = {0,0,0};
+	uint16_t bytes_used = 0;
+	textfile.read((char*)&code, sizeof(code));
+	int x = 1;
+	while (size >= 2)
 	{
-		textfile.read((char*)&code, 2);
-		msp430Decompose(code);
+		instruction set;
+		memset(&set, 0, sizeof(set));
+		bytes_used = msp430Decompose(code, set);
+		output(code, bytes_used, set);
+		size -= bytes_used;
+		memmove(&code[0], &code[bytes_used/2], 6-bytes_used);
+		textfile.read((char*)&code[(6-bytes_used)/2], bytes_used);
 	}
+	return 0;
 }
-
-
-
 
 
 
